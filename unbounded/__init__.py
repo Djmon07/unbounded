981c105f3f75225ddf1484c5e7adbc5033bc7985
Daniel Monville <djmonwork@gmail.com>
2020-03-25 14:32:36 -0400

    redirected the html and imported db

--- unbounded/__init__.py
+++ unbounded/__init__.py
@@ -1,4 +1,7 @@
+from . import db
+from . import auth
 from flask import Flask
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -10,10 +13,23 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/')
     def index():
-        return 'Hello Word.'
+        return render_template('base.html')
+
+    from . import db
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    from . import auth
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
